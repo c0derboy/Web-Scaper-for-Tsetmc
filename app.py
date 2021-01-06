d9bdf4b6b807fe0d4c6745f97de50cc5839ba3b8
c0derboy <per3ian.killer.recover@gmail.com>
2021-01-06 12:52:43 +0330

    V1.1 added calcuting 10 day trend

--- app.py
+++ app.py
@@ -36,15 +36,33 @@ hf = [i[4:-5] for i in hf]
 hf = [float(i) for i in hf]
 rng = hf[0] - hf[1]
 movement = rng / rf * 100
-final_text = f"{weekdays[weekday]}, {day} of {months[month - 1]}:\tHigh: {hf[0]:,}\tLow: {hf[1]:,}\tLast: {rf:,}\t Range: {rng:,.2f}\t Fluctuation: {movement:.2f}%\n"
-print(final_text)
+final_text = f"{weekdays[weekday]}, {day} of {months[month - 1]}:\tHigh: {hf[0]:,}\tLow: {hf[1]:,}\tLast: {rf:,}\t Range: {rng:,.2f}\t Fluctuation: {movement:.2f}%"
+print(f"High: {hf[0]:,}\tLow: {hf[1]:,}\tLast: {rf:,}\t Range: {rng:,.2f}\t Fluctuation: {movement:.2f}%")
+
+#Calculate the trend
+past_data = soup.find_all(class_='table1')
+past_data = past_data[1]
+past_data = past_data.find_all('td')[-3]
+past_data = str(past_data)
+past_data = past_data[4:-5]
+past_data = past_data.replace(',', '')
+past_data = float(past_data)
+
+trend10days = rf - past_data
+trendfluc = trend10days / rf * 100
+
+trend_text = f'In the past 10 days the market has moved by {round(trend10days /1000 ) * 1000} which is {trendfluc:.2f}%'
+print(trend_text)
 
 #Write the data into a txt file
 data = open('stockdata.txt', 'a+')
-data.write(final_text)
+data.write(f'{final_text}\n')
+data.write(f'{final_text}\n\n')
 data.close()
 
 """
+Features to work on:
 1.Automate for every X Hours
 2. Increase the number of pages + variables
+3. Calculate the trend in 2 weeks
 """
